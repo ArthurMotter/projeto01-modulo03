374a10ab3e5863ac96c19bb4b0ed05e93285bb2c
Arthur Motter <arthur.motter@fatec.sp.gov.br>
2025-07-21 15:32:34 -0300

    courses controller + get

---
+++ student-backend/src/main/java/com/abutua/student_backend/resources/CourseController.java
@@ -0,0 +1,28 @@
+package com.abutua.student_backend.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.abutua.student_backend.models.Course;
+
+@RestController
+public class CourseController {
+
+    private List<Course> courses = new ArrayList<>();
+
+    // Init courses
+    public CourseController() {
+        courses.add(new Course(1, "Ciência da Computação"));
+        courses.add(new Course(2, "Engenharia de Software"));
+        courses.add(new Course(3, "Sistemas de Informação"));
+    }
+
+    // Get all courses
+    @GetMapping("/courses")
+    public List<Course> getCourses() {
+        return this.courses;
+    }
+}
