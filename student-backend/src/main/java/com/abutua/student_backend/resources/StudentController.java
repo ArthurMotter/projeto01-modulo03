4033c5dde18be43a0e0c5f621d675830e46587e5
Arthur Motter <arthur.motter@fatec.sp.gov.br>
2025-07-21 15:50:56 -0300

    student creation

--- student-backend/src/main/java/com/abutua/student_backend/resources/StudentController.java
+++ student-backend/src/main/java/com/abutua/student_backend/resources/StudentController.java
@@ -1,5 +1,6 @@
 package com.abutua.student_backend.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,21 +8,33 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.abutua.student_backend.models.Student;
 
 @RestController
 public class StudentController {
 
+    /* Students Instances */
     private List<Student> students = new ArrayList<>();
 
-    // Init students
-    public StudentController() {
-        students.add(new Student(1, "Ana Silva", "ana.silva@example.com", "11987654321", 1, 3));
-        students.add(new Student(2, "Bruno Costa", "bruno.costa@example.com", "21912345678", 2, 1));
-        students.add(new Student(3, "Carlos Dias", "carlos.dias@example.com", "31988887777", 1, 5));
+    /* Save an specific student by request */
+    @PostMapping("students")
+    public ResponseEntity<Student> save(@RequestBody Student student) {
+        student.setId(students.size() + 1);
+        students.add(student);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(student.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(student);
     }
 
     /* Return an specific student by its ID */
